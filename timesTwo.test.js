afc76b66aa9b3883b298161ccded8d4322b90db2
Oladimeji Abdrahman Adebayo <67334984+rahmlad-aramide@users.noreply.github.com>
2022-07-12 14:28:17 +0000

    Final Commit

---
+++ timesTwo.test.js
@@ -0,0 +1,9 @@
+const { default: Testrunner } = require("jest-runner");
+const timesTwo = require('./timesTwo');
+
+// Write the first test
+test('returns the number times 2', () => {
+    expect(timesTwo(10)).toBe(20);
+});
+
+
